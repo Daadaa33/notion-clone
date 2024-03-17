c1757f154b89c32206fb20dbcbe66bb3086ef236
Abdikadir qulle <wlaaldaadaa33@gmail.com>
2024-03-17 12:13:56 +0300

    User dropdown

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,34 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { useUser } from '@clerk/clerk-react'
+import { PlusCircle } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 const DocumentsPage = () => {
+
+    const { user } = useUser()
   return (
-    <div>Document Page</div>
+    <div className='h-full flex flex-col justify-center items-center space-y-4'>
+        <Image src="/empty.png"
+         width="300"
+         height="300"
+         alt='emty'
+         className='dark:hidden'
+        />
+        <Image src="/empty-dark.png"
+         width="300"
+         height="300"
+         alt='emty'
+         className='hidden dark:block'
+        />
+        <h2 className='text-lg font-medium'>welcome {user?.fullName}&apos;s to Jotion</h2>
+        <Button>
+            <PlusCircle className='h-4 w-4 mr-2' />
+            create new note
+        </Button>
+    </div>
   )
 }
 
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import {  ChevronsLeft, MenuIcon } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import React, { ElementRef, useRef, useState } from 'react'
 import { useMediaQuery } from 'usehooks-ts'
+import {UserItem} from './user-item'
 
 const Navigation = () => {
     const pathname = usePathname()
@@ -85,13 +86,13 @@ const Navigation = () => {
         ref={sidebarRef}
         className={cn(
           "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
-          isResetting && "transition-all ease-in-out duration-300 ",
+          isResetting && "transition-all ease-in-out duration-300",
           isMobile && "w-0"
         )}
       >
         <div
-          role="button"
           onClick={collapse}
+          role="button"
           className={cn(
             "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
             isMobile && "opacity-100"
@@ -100,26 +101,31 @@ const Navigation = () => {
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Actions Items</p>
+          <UserItem />
         </div>
         <div className="mt-4  ">Documents</div>
-        
         <div
-        onMouseDown={handleMouseDown}
-        onClick={resetWidth}
-        className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" 
+          onMouseDown={handleMouseDown}
+          onClick={resetWidth}
+          className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
         />
       </aside>
-
       <div
+        ref={navbarRef}
         className={cn(
-          "absolute top-0 z-[99999] left-60  w-[calc(100% - 240px)] ",
+          "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "left-0 w-full"
         )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && <MenuIcon role="button" className="h-6 w-6" />}
+          {isCollapsed && (
+            <MenuIcon
+              onClick={resetWidth}
+              role="button"
+              className="h-6 w-6 text-muted-foreground"
+            />
+          )}
         </nav>
       </div>
     </>
