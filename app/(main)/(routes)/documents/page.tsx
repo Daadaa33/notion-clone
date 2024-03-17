8aa895dcb3c95a1d559539c82961bdb4887f310c
Abdikadir qulle <wlaaldaadaa33@gmail.com>
2024-03-17 12:49:45 +0300

    schema & api

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,12 +1,27 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api'
 import { useUser } from '@clerk/clerk-react'
+import { useMutation } from 'convex/react'
 import { PlusCircle } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
+import { toast } from 'sonner'
 
 const DocumentsPage = () => {
+
+    const create = useMutation(api.documents.create)
+
+    const onCreate = () => {
+        const promise = create({ title : "2023-204" })
+        
+        toast.promise(promise, {
+            loading: "Creating a new note...",
+            success: "New note created!",
+            error: "Failed to create new note!"
+        })
+    }
 
     const { user } = useUser()
   return (
@@ -24,7 +39,7 @@ const DocumentsPage = () => {
          className='hidden dark:block'
         />
         <h2 className='text-lg font-medium'>welcome {user?.fullName}&apos;s to Jotion</h2>
-        <Button>
+        <Button onClick={onCreate}> 
             <PlusCircle className='h-4 w-4 mr-2' />
             create new note
         </Button>
